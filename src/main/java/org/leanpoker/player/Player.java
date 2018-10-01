579fa19abacc6ed7951f545c3d17c54e22df6e20
anitaSzabo <azorka3@gmail.com>
2018-10-01 10:28:21 +0200

    random bet

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,13 +3,16 @@ package org.leanpoker.player;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
+import java.util.Random;
 
 public class Player {
 
     static final String VERSION = "0.1";
 
     public static int betRequest(JsonElement request) {
-        return 2;
+
+        Random random = new Random();
+        return random.nextInt(5);
     }
 
     public static void showdown(JsonElement game) {
