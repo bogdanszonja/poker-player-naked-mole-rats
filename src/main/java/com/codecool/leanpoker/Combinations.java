036fbbe5533a22bd892b549d9c332fa5f15f53ce
anitaSzabo <azorka3@gmail.com>
2018-10-01 15:36:56 +0200

    Is3OfAKind

--- src/main/java/com/codecool/leanpoker/Combinations.java
+++ src/main/java/com/codecool/leanpoker/Combinations.java
@@ -21,4 +21,15 @@ public class Combinations {
         return false;
     }
 
+    public static boolean is3OfAKind(List<Card> myCards, List<Card> cardsOnDeck) {
+        for (Card cardDeck : cardsOnDeck) {
+            if (isTheSameRank(myCards.get(0), myCards.get(1)) && isTheSameRank(myCards.get(0), cardDeck) ||
+                    isTheSameRank(myCards.get(0), myCards.get(1)) && isTheSameRank(myCards.get(1), cardDeck)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 }
