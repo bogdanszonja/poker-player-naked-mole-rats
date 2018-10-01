9d7de0f62321f6ca06124873334dba951bce2913
Grob Oliver <groboliver1117@gmail.com>
2018-10-01 13:56:09 +0200

    Card class

--- src/main/java/com/codecool/leanpoker/Card.java
+++ src/main/java/com/codecool/leanpoker/Card.java
@@ -1,4 +1,27 @@
 package com.codecool.leanpoker;
 
 public class Card {
+
+    private Suit suit;
+    private int value;
+
+
+    public Card(String suit, String value) {
+        this.suit = Suit.valueOf(suit);
+        try {
+            this.value = Integer.parseInt(value);
+        } catch (Exception e) {
+            this.value = Rank.getValue(Rank.valueOf(value));
+        }
+    }
+
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }
