9e4dd90ed550d0b8ce15bdd850d54d1ecd097860
Grob Oliver <groboliver1117@gmail.com>
2018-10-01 15:39:15 +0200

    more pairs

--- src/main/java/com/codecool/leanpoker/Combinations.java
+++ src/main/java/com/codecool/leanpoker/Combinations.java
@@ -21,4 +21,23 @@ public class Combinations {
         return false;
     }
 
+    public static boolean morePairAt3Cards(List<Card> myCards, List<Card> cardsOnDeck) {
+        if (Combinations.isTheSameRank(myCards.get(0), myCards.get(1))) {
+            return false;
+        }
+
+        int currentPairNum = 0;
+
+        for (Card cardHand : myCards) {
+            for (Card cardDeck : cardsOnDeck) {
+                if (Combinations.isTheSameRank(cardHand, cardDeck)) {
+                    currentPairNum++;
+                    break;
+                }
+            }
+        }
+
+        return currentPairNum >= 2;
+    }
+
 }
